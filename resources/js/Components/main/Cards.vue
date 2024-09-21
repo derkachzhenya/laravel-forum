f11dcd9af4c5c6c0e5575e888f135ff298950ff4
Yevhen <zheka4573@gmail.com>
2024-09-21 14:24:06 +0300

    v2.7

--- resources/js/Components/main/Cards.vue
+++ resources/js/Components/main/Cards.vue
@@ -11,14 +11,12 @@
             <!--EndText-->
         </div>
         <div class="mt-5">
-            <div
-                class="grid mb-8 border border-gray-200 rounded-lg shadow-sm md:mb-12 md:grid-cols-2 bg-white">
+            <div class="grid mb-8 border border-gray-200 rounded-lg shadow-sm md:mb-12 md:grid-cols-2 bg-white">
                 <figure
                     class="flex flex-col items-center justify-center p-8 text-center bg-white border-gray-200 rounded-t-lg md:rounded-t-none md:rounded-ss-lg md:border-e">
                     <blockquote class="max-w-2xl mx-auto mb-4 text-gray-500 lg:mb-8 dark:text-gray-400">
                         <a href="#">
-                            <h3
-                                class="text-xl font-bold text-gray-800 hover:text-gray-900 hover:underline">
+                            <h3 class="text-xl font-bold text-gray-800 hover:text-gray-900 hover:underline">
                                 Puzata
                                 hata
                             </h3>
@@ -205,17 +203,25 @@
     </section>
     <!--EndCards-->
     <div class="grid w-3/4 mx-auto justify-items-end">
-    <a href="#"
-        class="inline-flex items-center justify-center p-5 text-base font-medium text-gray-500 rounded-lg bg-gray-100 border hover:text-gray-900 hover:bg-gray-200 dark:text-gray-400 dark:hover:text-white">
-        <svg aria-hidden="true" class="w-6 h-6 me-3" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M21 10C21 17 12 23 12 23C12 23 3 17 3 10C3 7.61305 3.94821 5.32387 5.63604 3.63604C7.32387 1.94821 9.61305 1 12 1C14.3869 1 16.6761 1.94821 18.364 3.63604C20.0518 5.32387 21 7.61305 21 10Z" fill="#FF6347"/>
-            <path d="M12 13C13.6569 13 15 11.6569 15 10C15 8.34315 13.6569 7 12 7C10.3431 7 9 8.34315 9 10C9 11.6569 10.3431 13 12 13Z" fill="#FFA07A"/>
-            <path d="M7 19H17M9 15L12 17L15 15" stroke="#4A5568" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-        </svg>
-        <span class="w-full">View all restaurants and eateries</span>
-        <svg class="w-4 h-4 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
-        </svg>
-    </a>
-</div>
+        <a href="#"
+            class="inline-flex items-center justify-center p-5 text-base font-medium text-gray-500 rounded-lg bg-gray-100 border hover:text-gray-900 hover:bg-gray-200 dark:text-gray-400 dark:hover:text-white">
+            <svg aria-hidden="true" class="w-6 h-6 me-3" viewBox="0 0 24 24" fill="none"
+                xmlns="http://www.w3.org/2000/svg">
+                <path
+                    d="M21 10C21 17 12 23 12 23C12 23 3 17 3 10C3 7.61305 3.94821 5.32387 5.63604 3.63604C7.32387 1.94821 9.61305 1 12 1C14.3869 1 16.6761 1.94821 18.364 3.63604C20.0518 5.32387 21 7.61305 21 10Z"
+                    fill="#FF6347" />
+                <path
+                    d="M12 13C13.6569 13 15 11.6569 15 10C15 8.34315 13.6569 7 12 7C10.3431 7 9 8.34315 9 10C9 11.6569 10.3431 13 12 13Z"
+                    fill="#FFA07A" />
+                <path d="M7 19H17M9 15L12 17L15 15" stroke="#4A5568" stroke-width="2" stroke-linecap="round"
+                    stroke-linejoin="round" />
+            </svg>
+            <span class="w-full">View all restaurants and eateries</span>
+            <svg class="w-4 h-4 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                viewBox="0 0 14 10">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                    d="M1 5h12m0 0L9 1m4 4L9 9" />
+            </svg>
+        </a>
+    </div>
 </template>
--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -39,7 +39,7 @@ import Footer from '@/Components/main/Footer.vue';
             curated guide to the world's best hotels and shops.</p>
         <!--EndTwoText-->
 
-        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 w-full sm:w-3/4 sm:mx-auto pt-4 pb-8">
+        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 w-full sm:w-3/4 sm:mx-auto pt-4">
 
             <div class="flex justify-center mb-5 mr-3 ml-3">
                 <div class="w-full sm:w-64">
@@ -112,6 +112,17 @@ import Footer from '@/Components/main/Footer.vue';
                     </div>
                 </div>
             </div>
+        </div>
+        <div class="w-3/4 mx-auto text-end pt-3 pb-10 pe-3">
+            <a href="#"
+                class="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg
+                 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-100 focus:text-blue-700
+                  dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700
+                   dark:focus:ring-gray-700">Show all <svg class="w-3 h-3 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                    fill="none" viewBox="0 0 14 10">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        d="M1 5h12m0 0L9 1m4 4L9 9" />
+                </svg></a>
         </div>
     </div>
     <!--EndBackground-->

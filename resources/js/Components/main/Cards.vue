f7211fde4150466b0cb6c3bd22d751133bb222da
Yevhen <zheka4573@gmail.com>
2024-09-21 09:56:54 +0300

    v2.7

--- resources/js/Components/main/Cards.vue
+++ resources/js/Components/main/Cards.vue
@@ -12,7 +12,7 @@
         </div>
         <div class="mt-5">
             <div
-                class="grid mb-8 border border-gray-200 rounded-lg shadow-sm dark:border-gray-700 md:mb-12 md:grid-cols-2 bg-white dark:bg-gray-800">
+                class="grid mb-8 border border-gray-200 rounded-lg shadow-sm dark:border-gray-700 md:mb-12 md:grid-cols-2 bg-white">
                 <figure
                     class="flex flex-col items-center justify-center p-8 text-center bg-white border-gray-200 rounded-t-lg md:rounded-t-none md:rounded-ss-lg md:border-e dark:bg-gray-800 dark:border-gray-700">
                     <blockquote class="max-w-2xl mx-auto mb-4 text-gray-500 lg:mb-8 dark:text-gray-400">
@@ -204,40 +204,18 @@
         </div>
     </section>
     <!--EndCards-->
-    <div class = "grid w-3/4 mx-auto justify-items-end">
-        <a href="#"
-            class="inline-flex items-center justify-center p-5 text-base font-medium text-gray-500 rounded-lg bg-gray-100 hover:text-gray-900 hover:bg-gray-200 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700 dark:hover:text-white">
-            <svg aria-hidden="true" class="w-5 h-5 me-3" viewBox="0 0 22 31" fill="none"
-                xmlns="http://www.w3.org/2000/svg">
-                <g clip-path="url(#clip0_4151_63004)">
-                    <path
-                        d="M5.50085 30.1242C8.53625 30.1242 10.9998 27.8749 10.9998 25.1035V20.0828H5.50085C2.46546 20.0828 0.00195312 22.332 0.00195312 25.1035C0.00195312 27.8749 2.46546 30.1242 5.50085 30.1242Z"
-                        fill="#0ACF83" />
-                    <path
-                        d="M0.00195312 15.062C0.00195312 12.2905 2.46546 10.0413 5.50085 10.0413H10.9998V20.0827H5.50085C2.46546 20.0827 0.00195312 17.8334 0.00195312 15.062Z"
-                        fill="#A259FF" />
-                    <path
-                        d="M0.00195312 5.02048C0.00195312 2.24904 2.46546 -0.000244141 5.50085 -0.000244141H10.9998V10.0412H5.50085C2.46546 10.0412 0.00195312 7.79193 0.00195312 5.02048Z"
-                        fill="#F24E1E" />
-                    <path
-                        d="M11 -0.000244141H16.4989C19.5343 -0.000244141 21.9978 2.24904 21.9978 5.02048C21.9978 7.79193 19.5343 10.0412 16.4989 10.0412H11V-0.000244141Z"
-                        fill="#FF7262" />
-                    <path
-                        d="M21.9978 15.062C21.9978 17.8334 19.5343 20.0827 16.4989 20.0827C13.4635 20.0827 11 17.8334 11 15.062C11 12.2905 13.4635 10.0413 16.4989 10.0413C19.5343 10.0413 21.9978 12.2905 21.9978 15.062Z"
-                        fill="#1ABCFE" />
-                </g>
-                <defs>
-                    <clipPath id="clip0_4151_63004">
-                        <rect width="22" height="30.1244" fill="white" transform="translate(0 -0.000244141)" />
-                    </clipPath>
-                </defs>
-            </svg>
-            <span class="w-full">View all restaurants and eateries</span>
-            <svg class="w-4 h-4 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
-                viewBox="0 0 14 10">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                    d="M1 5h12m0 0L9 1m4 4L9 9" />
-            </svg>
-        </a>
-    </div>
+    <div class="grid w-3/4 mx-auto justify-items-end">
+    <a href="#"
+        class="inline-flex items-center justify-center p-5 text-base font-medium text-gray-500 rounded-lg bg-gray-100 hover:text-gray-900 hover:bg-gray-200 dark:text-gray-400 dark:hover:text-white">
+        <svg aria-hidden="true" class="w-6 h-6 me-3" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M21 10C21 17 12 23 12 23C12 23 3 17 3 10C3 7.61305 3.94821 5.32387 5.63604 3.63604C7.32387 1.94821 9.61305 1 12 1C14.3869 1 16.6761 1.94821 18.364 3.63604C20.0518 5.32387 21 7.61305 21 10Z" fill="#FF6347"/>
+            <path d="M12 13C13.6569 13 15 11.6569 15 10C15 8.34315 13.6569 7 12 7C10.3431 7 9 8.34315 9 10C9 11.6569 10.3431 13 12 13Z" fill="#FFA07A"/>
+            <path d="M7 19H17M9 15L12 17L15 15" stroke="#4A5568" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+        </svg>
+        <span class="w-full">View all restaurants and eateries</span>
+        <svg class="w-4 h-4 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+        </svg>
+    </a>
+</div>
 </template>
--- resources/js/Components/main/Footer.vue
+++ resources/js/Components/main/Footer.vue
@@ -9,7 +9,7 @@
          <div class="sm:flex sm:items-center sm:justify-between">
              <a href="https://flowbite.com/" class="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
                  <img :src="'/airplane.png'" class="h-8" alt="Flowbite Logo" />
-                 <span class="self-center text-2xl font-semibold whitespace-nowrap">Travel</span>
+                 <span class="self-center text-2xl font-semibold whitespace-nowrap">FoodHub</span>
              </a>
              <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
                  <li>
@@ -28,7 +28,7 @@
          </div>
          <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
          <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">©  <a
-                 href="https://travel.com/" class="hover:underline">Travel™</a>. All Rights Reserved.</span>
+                 href="https://travel.com/" class="hover:underline">FoodHub™</a>. All Rights Reserved.</span>
      </div>
  </footer>
  <!--EndFooter-->
